1ec053b8ae4292f630d57365fa2c871157532f3e
Victor Wohangara <ijoo.keren@gmail.com>
2024-08-26 12:12:07 +0700

    Update GetIP.py

--- GetIP.py
+++ GetIP.py
@@ -1,6 +1,7 @@
 # This Python Script Sample For Eggdrop
 # Eggdrop 1.9.6+ with python module needed.
 # Simple IP Check Sripting
+# Author = iJoo - evochat.id / #metro
 
 from eggdrop import bind
 from eggdrop.tcl import putmsg, putlog
@@ -8,7 +9,6 @@ import requests
 
 def get_location(nick, user, hand, chan, text, **kwargs):
     ip_address = text or "8.8.8.8"
-    putlog("Mencari IP "+text)
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     kota = response.get('city')
     region = response.get('region')
@@ -17,6 +17,6 @@ def get_location(nick, user, hand, chan, text, **kwargs):
     orgnya = response.get('org')
     putmsg(chan, f"\002IP:\002 "+ ip_address)
     putmsg(chan, f"\002Location:\002 " + kota + ", " + region + " (" + negara + ")")
-    putmsg(chan, f"\002ASN:\002 " + asnnya + " (" +orgnya+ ")")
+    putmsg(chan, f"\002ASN:\002 " + asnnya + " (" + orgny a+ ")")
 
 bind("pub", "*", "!ip", get_location)
