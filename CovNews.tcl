3a66ea2b9c221f73be10a838cb141120f808355a
This is iJOo <ijoo.keren@gmail.com>
2026-09-04 07:06:30 +0700

    Update CovNews.tcl

--- CovNews.tcl
+++ CovNews.tcl
@@ -10,18 +10,23 @@
 # maxberita - berapa banyak berita dalam satu kategori [ min 1, max disarankan 4 ]
 ##########
 
-set ijoo(newschan) "#metro"
 set ijoo(maxberita) 2
-set ijoo(newslogo) [dezip "tw9TS1XklaY.fKP711agSxk//WzEX/WpdGs1zpX6//wAcV60"]
+set ijoo(detiklogo) [dezip "IjDB3/ygPBT1Oi1Sn.0uHg21mmO1i.aMC8F/ceCBi/sMIbx/OgAQ0/IEE29.PzSIW0c035C/"]
+set ijoo(liputanlogo) [dezip "27z7Z08Qwc91Y7Ewe0I2wZN.G8xEo1XAZbu13Kvlf/sYUoM1rk4ar.9eNZm0"]
+set ijoo(cnnlogo) [dezip "NtUTS0plS0q0eLm/I1bEgxa0VKfY3.HQa56.5P2Th/mnzoW0StamL1OsZy3/"]
+set ijoo(republikalogo) [dezip "G243f1GO9cN/9AqFU/SDZTK/wacVV0T6k4M/oIdAm1y0eL9/ceS9A/KMbrX1CZXy91NXYmQ0"]
 
-bind time -|- {45 * * * *} go_berita
+bind time -|- {00 * * * *} get_rdm
+bind time -|- {30 * * * *} get_rdm
+
+setudef flag berita
 
 ########## PACKAGE
 package require http
 package require tdom
 package require tls
 ::tls::init -ssl2 false -ssl3 false -tls1 true
-::http::register https 443 ::tls::socket
+::http::register https 443 [list ::tls::socket -autoservername true -require true -cadir /etc/ssl/certs]
 ::http::config -useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"
 
 set a_berita {
@@ -32,34 +37,171 @@ set a_berita {
         "Kabar dari"
 }
 
-proc go_berita {args} {
-        set detik_kat { "health" "news" "inet" "finance" "hot" "sport" "travel" "food" "wolipop" }
-        set kat [lindex $detik_kat [rand [llength $detik_kat]]]
-        putlog "############# Get Detik-$kat !"
-        get_berita $kat
+
+proc get_rdm {args} {
+        set news_src {"go_detik" "go_cnn" "go_liputan" "go_republika"}
+        set news_on [lindex $news_src [rand [llength $news_src]]]
+        $news_on
 }
 
-proc get_berita {cat} {
-        global ijoo a_berita
-        set mx 0
-        set urlnews https://$cat.detik.com/rss
-        set srcout [http::data [http::geturl $urlnews]]
-        set dom [dom parse $srcout]
-        set root [$dom documentElement]
-        foreach node [$root selectNodes "/rss/channel/item"] {
-                if { $mx == $ijoo(maxberita) } { break }
-                set mx [expr {$mx + 1}]
-                set judul [[lindex [$node selectNodes title] 0] text]
-                set linkberita [[lindex [$node selectNodes guid] 0] text]
-                set b_berita [lindex $a_berita [rand [llength $a_berita]]]
-                if { $mx > 1 } {
-                        putquick "PRIVMSG $ijoo(newschan) :$ijoo(newslogo) \003Artikel berikutnya [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+proc go_detik {args} {
+        global notic
+        set channels_string [channels]
+        set cleaned_string [regsub -all {[{}]} $channels_string ""]
+        set channel_list [split $cleaned_string " "]
+        foreach c $channel_list {
+                if {![channel get $c berita]} { 
+                        putlog "$notic $c is Not Berita Channel"
                 } else {
-                        putquick "PRIVMSG $ijoo(newschan) :$ijoo(newslogo) \003[salam], $b_berita \0038,12 [string toupper $kat] \003 [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                        putlog "$notic $c is Berita Channel -> Send News!"
+                        set detik_kat { "health" "news" "inet" "finance" "hot" "sport" "travel" "food" "wolipop" }
+                        set kat [lindex $detik_kat [rand [llength $detik_kat]]]
+                        putlog "$notic Get News Detik from $kat !"
+                        get_detik $kat $c
                 }
         }
-        putlog "############# CleanUP News!!"
-        ::http::cleanup $srcout
+}
+
+proc go_liputan {args} {
+        global notic
+        set channels_string [channels]
+        set cleaned_string [regsub -all {[{}]} $channels_string ""]
+        set channel_list [split $cleaned_string " "]
+        foreach c $channel_list {
+                if {![channel get $c berita]} { 
+                        putlog "$notic $c is Not Berita Channel"
+                } else {
+                        putlog "$notic $c is Berita Channel -> Send News!"
+                        set liputan_kat { "bisnis" "news" "tv" "showbiz" "tekno" "hot" "cek-fakta" "crypto" "saham" "otomotif"}
+                        set kat [lindex $liputan_kat [rand [llength $liputan_kat]]]
+                        putlog "$notic Get News Liputan6 from $kat !"
+                        get_liputan $kat $c
+                }
+        }
+}
+
+proc go_cnn {args} {
+        global notic
+        set channels_string [channels]
+        set cleaned_string [regsub -all {[{}]} $channels_string ""]
+        set channel_list [split $cleaned_string " "]
+        foreach c $channel_list {
+                if {![channel get $c berita]} { 
+                        putlog "$notic $c is Not Berita Channel"
+                } else {
+                        putlog "$notic $c is Berita Channel -> Send News!"
+                        set cnn_kat { "ekonomi" "teknologi" "hiburan" "otomotif" "gaya-hidup" "internasional"}
+                        set kat [lindex $cnn_kat [rand [llength $cnn_kat]]]
+                        putlog "$notic Get News CNNIndonesia from $kat !"
+                        get_cnn $kat $c
+                }
+        }
+}
+
+proc go_republika {args} {
+        global notic
+        set channels_string [channels]
+        set cleaned_string [regsub -all {[{}]} $channels_string ""]
+        set channel_list [split $cleaned_string " "]
+        foreach c $channel_list {
+                if {![channel get $c berita]} { 
+                        putlog "$notic $c is Not Berita Channel"
+                } else {
+                        putlog "$notic $c is Berita Channel -> Send News!"
+                        set repb_kat { "news" "finansial" "tekno" "otomotif" "bisnis" "sport"}
+                        set kat [lindex $repb_kat [rand [llength $repb_kat]]]
+                        putlog "$notic Get News REPUBLIKA from $kat !"
+                        get_republika $kat $c
+                }
+        }
+}
+
+proc get_detik {cat chn} {
+        global ijoo a_berita
+        set mx 0
+        set urlnews "https://$cat.detik.com/rss"
+        set rssData [get_news $urlnews]
+        set doc [dom parse $rssData]
+        set root [$doc documentElement]
+        set items [$root selectNodes //item]
+        foreach node $items {
+                if { $mx == $ijoo(maxberita) } { break }
+                set mx [expr {$mx + 1}]
+                set judul [[[$node selectNodes title] firstChild] nodeValue]
+                set linkberita [[[$node selectNodes link] firstChild] nodeValue]
+                set b_berita [lindex $a_berita [rand [llength $a_berita]]]
+                if { $mx > 1 } {
+                        putquick "PRIVMSG $chn :$ijoo(detiklogo) \003Artikel berikutnya [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                } else {
+                        putquick "PRIVMSG $chn :$ijoo(detiklogo) \003[salam], $b_berita \0038,12 [string toupper $cat] \003 [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                }
+        }
+}
+
+proc get_liputan {cat chn} {
+        global ijoo a_berita
+        set mx 0
+        set urlnews "https://feed.liputan6.com/rss/$cat"
+        set rssData [get_news $urlnews]
+        set doc [dom parse $rssData]
+        set root [$doc documentElement]
+        set items [$root selectNodes //item]
+        foreach node $items {
+                if { $mx == $ijoo(maxberita) } { break }
+                set mx [expr {$mx + 1}]
+                set judul [[[$node selectNodes title] firstChild] nodeValue]
+                set linkberita [[[$node selectNodes link] firstChild] nodeValue]
+                set b_berita [lindex $a_berita [rand [llength $a_berita]]]
+                if { $mx > 1 } {
+                        putquick "PRIVMSG $chn :$ijoo(liputanlogo) \003Artikel berikutnya [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                } else {
+                        putquick "PRIVMSG $chn :$ijoo(liputanlogo) \003[salam], $b_berita \0030,4 [string toupper $cat] \003 [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                }
+        }
+}
+
+proc get_cnn {cat chn} {
+        global ijoo a_berita
+        set mx 0
+        set urlnews "https://www.cnnindonesia.com/$cat/rss"
+        set rssData [get_news $urlnews]
+        set doc [dom parse $rssData]
+        set root [$doc documentElement]
+        set items [$root selectNodes //item]
+        foreach node $items {
+                if { $mx == $ijoo(maxberita) } { break }
+                set mx [expr {$mx + 1}]
+                set judul [[[$node selectNodes title] firstChild] nodeValue]
+                set linkberita [[[$node selectNodes link] firstChild] nodeValue]
+                set b_berita [lindex $a_berita [rand [llength $a_berita]]]
+                if { $mx > 1 } {
+                        putquick "PRIVMSG $chn :$ijoo(cnnlogo) \003Artikel berikutnya [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                } else {
+                        putquick "PRIVMSG $chn :$ijoo(cnnlogo) \003[salam], $b_berita \0034,1 [string toupper $cat] \003 [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                }
+        }
+}
+
+proc get_republika {cat chn} {
+        global ijoo a_berita
+        set mx 0
+        set urlnews "https://www.republika.co.id/rss/$cat"
+        set rssData [get_news $urlnews]
+        set doc [dom parse $rssData]
+        set root [$doc documentElement]
+        set items [$root selectNodes //item]
+        foreach node $items {
+                if { $mx == $ijoo(maxberita) } { break }
+                set mx [expr {$mx + 1}]
+                set judul [[[$node selectNodes title] firstChild] nodeValue]
+                set linkberita [[[$node selectNodes link] firstChild] nodeValue]
+                set b_berita [lindex $a_berita [rand [llength $a_berita]]]
+                if { $mx > 1 } {
+                        putquick "PRIVMSG $chn :$ijoo(republikalogo) \003Artikel berikutnya [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                } else {
+                        putquick "PRIVMSG $chn :$ijoo(republikalogo) \003[salam], $b_berita \0030,1 [string toupper $cat] \003 [dezip ivRvn.ZAO1G/] \00312$judul\003 [dezip ivRvn.ZAO1G/] Selengkapnya di \037[tinyurl $linkberita]\037"
+                }
+        }
 }
 
 proc tinyurl {url} {
@@ -87,9 +229,11 @@ proc salam {} {
         return $slm
 }
 
-if {[catch {source scripts/Covid.tcl} err]} {
-        putlog "############# WARN: Cannot load Covid.tcl"
-        putlog "############# Download https://github.com/ijoo/covid"
+proc get_news {target} {
+    set token [::http::geturl $target]
+    set body [::http::data $token]
+    ::http::cleanup $token
+    return $body
 }
 
 putlog "#######################################"
